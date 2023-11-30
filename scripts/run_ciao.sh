a7472d6696b59f0c48c2c44c913a6d1c04f604e1
Zongwei9888 <145887958+Zongwei9888@users.noreply.github.com>
2023-11-30 11:53:50 +0800

    Update run_ciao.sh

--- scripts/run_ciao.sh
+++ scripts/run_ciao.sh
@@ -1,2 +1,2 @@
 python main.py --n_hid 64 --dataset ciao --n_layers 2 --s_layer 3 --lr 0.01 --difflr 0.001 --reg 0.01 --batch_size 2048 --test_batch_size 2048 \
- --emb_size 4 --steps 20 --noise_scale 1 --model_dir './Models/ciao/'
+ --emb_size 4 --steps 20 --noise_scale 1 --model_dir './Model/ciao/'
